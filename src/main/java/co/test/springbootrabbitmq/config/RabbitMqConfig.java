1004e3c7e0f034ed13ba7e872a94172b59a0901a
shubhamsadare6626 <shubhamsadaress@gmail.com>
2024-07-13 15:00:46 +0530

    modified pom file and spotless applied

--- src/main/java/co/test/springbootrabbitmq/config/RabbitMqConfig.java
+++ src/main/java/co/test/springbootrabbitmq/config/RabbitMqConfig.java
@@ -1,7 +1,13 @@
 package co.test.springbootrabbitmq.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.util.StdDateFormat;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import java.util.TimeZone;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
@@ -16,102 +22,98 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.util.StdDateFormat;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
-import lombok.RequiredArgsConstructor;
-
 @Configuration
 @EnableScheduling
 @RequiredArgsConstructor
 public class RabbitMqConfig {
-	
-	private final SystemConfig systemConfig;
 
-	@Bean
-	public Queue testQueue() {
-		return new Queue(systemConfig.getQueue());
-	}
+  private final SystemConfig systemConfig;
 
-	@Bean
-	public DirectExchange directExchange() {
-		return new DirectExchange(systemConfig.getExchange());
-	}
+  @Bean
+  public Queue testQueue() {
+    return new Queue(systemConfig.getQueue());
+  }
 
-	// Binding queue with exchange using routing key
-	@Bean
-	public Binding binding() {
-		return BindingBuilder.bind(testQueue()).to(directExchange()).with(systemConfig.getRoutingkey());
-	}
+  @Bean
+  public DirectExchange directExchange() {
+    return new DirectExchange(systemConfig.getExchange());
+  }
 
-	@Bean
-	public MessageConverter jsonMessageConverter() {
-		return new Jackson2JsonMessageConverter();
-	}
+  // Binding queue with exchange using routing key
+  @Bean
+  public Binding binding() {
+    return BindingBuilder.bind(testQueue()).to(directExchange()).with(systemConfig.getRoutingkey());
+  }
 
-	@Bean
-	public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
-		RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-		rabbitTemplate.setMessageConverter(jsonMessageConverter());
-		return rabbitTemplate;
-	}
+  @Bean
+  public MessageConverter jsonMessageConverter() {
+    return new Jackson2JsonMessageConverter();
+  }
 
-	// can create multiple queue
-	@Bean
-	public Queue test2Queue() {
-		return new Queue(systemConfig.getQueue2());
-	}
+  @Bean
+  public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+    RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+    rabbitTemplate.setMessageConverter(jsonMessageConverter());
+    return rabbitTemplate;
+  }
 
-	@Bean
-	public Binding test2binding() {
-		return BindingBuilder.bind(test2Queue()).to(directExchange()).with(systemConfig.getRoutingkey2());
-	}
+  // can create multiple queue
+  @Bean
+  public Queue test2Queue() {
+    return new Queue(systemConfig.getQueue2());
+  }
 
-	@Bean
-	public ObjectMapper objectMapper() {
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.registerModule(new JavaTimeModule());
-		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		mapper.setTimeZone(TimeZone.getDefault());
-		mapper.enable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-		mapper.setDateFormat(new StdDateFormat().withColonInTimeZone(true));
-		mapper.setPropertyNamingStrategy(new PropertyNamingStrategies.SnakeCaseStrategy());
-		return mapper;
-	}
-	
-	// Spring boot Auto configuration -Configures
-	// Connection Factory
-	// Rabbit template (C) and AMQP template (I)
+  @Bean
+  public Binding test2binding() {
+    return BindingBuilder.bind(test2Queue())
+        .to(directExchange())
+        .with(systemConfig.getRoutingkey2());
+  }
 
-//	==================== Topic Exchange ================================================
-	
-	@Bean
-	public Queue jsonQueue() {
-		return new Queue(systemConfig.getJsonQueue());
-	}
-	
-	@Bean
-	public Queue jsonEmailQueue() {
-		return new Queue(systemConfig.getJsonEmailQueue());
-	}
-	
-	@Bean
-	public TopicExchange topicExchange() {
-		return new TopicExchange(systemConfig.getJsonExchange());
-	}
-	
-	@Bean
-	public Binding jsonBinding() {
-		return BindingBuilder.bind(jsonQueue()).to(topicExchange()).with(systemConfig.getJsonRoutingkey());
-	}
+  @Bean
+  public ObjectMapper objectMapper() {
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.registerModule(new JavaTimeModule());
+    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    mapper.setTimeZone(TimeZone.getDefault());
+    mapper.enable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    mapper.setDateFormat(new StdDateFormat().withColonInTimeZone(true));
+    mapper.setPropertyNamingStrategy(new PropertyNamingStrategies.SnakeCaseStrategy());
+    return mapper;
+  }
 
-	@Bean
-	public Binding jsonEmailBinding() {
-		return BindingBuilder.bind(jsonEmailQueue()).to(topicExchange()).with(systemConfig.getJsonEmailRoutingkey());
-	}
+  // Spring boot Auto configuration -Configures
+  // Connection Factory
+  // Rabbit template (C) and AMQP template (I)
 
+  //	==================== Topic Exchange ================================================
+
+  @Bean
+  public Queue jsonQueue() {
+    return new Queue(systemConfig.getJsonQueue());
+  }
+
+  @Bean
+  public Queue jsonEmailQueue() {
+    return new Queue(systemConfig.getJsonEmailQueue());
+  }
+
+  @Bean
+  public TopicExchange topicExchange() {
+    return new TopicExchange(systemConfig.getJsonExchange());
+  }
+
+  @Bean
+  public Binding jsonBinding() {
+    return BindingBuilder.bind(jsonQueue())
+        .to(topicExchange())
+        .with(systemConfig.getJsonRoutingkey());
+  }
+
+  @Bean
+  public Binding jsonEmailBinding() {
+    return BindingBuilder.bind(jsonEmailQueue())
+        .to(topicExchange())
+        .with(systemConfig.getJsonEmailRoutingkey());
+  }
 }
--- src/main/java/co/test/springbootrabbitmq/consumer/MessgeConsumer.java
+++ src/main/java/co/test/springbootrabbitmq/consumer/MessgeConsumer.java
@@ -1,62 +1,61 @@
 package co.test.springbootrabbitmq.consumer;
 
-import org.springframework.amqp.AmqpException;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.stereotype.Service;
-
 import co.test.springbootrabbitmq.dto.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class MessgeConsumer {
 
-	/**
-	 * Assigns a Consumer to receive the messages whenever there is one
-	 *
-	 * @param message
-	 */
-	@RabbitListener(queues="${rabbitmq.test.queue}", priority="0", autoStartup = "true")
-	public void messageConsumer(String message) {
-		try {
-            log.info("Message received for test queue: {}", message);
-            //Perform Business logic
-        } catch (Exception ex) {
-            log.error("Failed to process message from test queue: {}", ex.getMessage(), ex);
-        }	
-	}
+  /**
+   * Assigns a Consumer to receive the messages whenever there is one
+   *
+   * @param message
+   */
+  @RabbitListener(queues = "${rabbitmq.test.queue}", priority = "0", autoStartup = "true")
+  public void messageConsumer(String message) {
+    try {
+      log.info("Message received for test queue: {}", message);
+      // Perform Business logic
+    } catch (Exception ex) {
+      log.error("Failed to process message from test queue: {}", ex.getMessage(), ex);
+    }
+  }
 
-	@RabbitListener(queues="${rabbitmq.test2.queue}", priority="1" ,autoStartup = "true")
-	public void secondMessageConsumer(String message) {
-		try {
-			log.info("Message received for test2 queue: {}",message);
-	    } catch (Exception ex) {
-            log.error("Failed to process message from test2: {}", ex.getMessage(), ex);
-        }
-	}
-	
-	/**
-	 * Assigns a Consumer to receive Json object received.
-	 *
-	 * @param user the user
-	 */
-	@RabbitListener(queues="${rabbitmq.json.queue}")
-	public void jsonObjectReceived(User user) {
-		try {
-			log.info("Message received of json-> {}", user);
-		} catch (AmqpException ex) {
-            log.error("Failed to process message from json: {}", ex.getMessage(), ex);
-		}
-	}
-	
-	@RabbitListener(queues="${rabbitmq.json.email-queue}")
-	public void jsonEmailReceived(User user) {
-		try {
-			log.info("Message received email from json-> {}", user.getEmail());
-		} catch (AmqpException ex) {
-            log.error("Failed to process message from json email: {}", ex.getMessage(), ex);
-		}
-	}
+  @RabbitListener(queues = "${rabbitmq.test2.queue}", priority = "1", autoStartup = "true")
+  public void secondMessageConsumer(String message) {
+    try {
+      log.info("Message received for test2 queue: {}", message);
+    } catch (Exception ex) {
+      log.error("Failed to process message from test2: {}", ex.getMessage(), ex);
+    }
+  }
+
+  /**
+   * Assigns a Consumer to receive Json object received.
+   *
+   * @param user the user
+   */
+  @RabbitListener(queues = "${rabbitmq.json.queue}")
+  public void jsonObjectReceived(User user) {
+    try {
+      log.info("Message received of json-> {}", user);
+    } catch (AmqpException ex) {
+      log.error("Failed to process message from json: {}", ex.getMessage(), ex);
+    }
+  }
+
+  @RabbitListener(queues = "${rabbitmq.json.email-queue}")
+  public void jsonEmailReceived(User user) {
+    try {
+      log.info("Message received email from json-> {}", user.getEmail());
+    } catch (AmqpException ex) {
+      log.error("Failed to process message from json email: {}", ex.getMessage(), ex);
+    }
+  }
 }
